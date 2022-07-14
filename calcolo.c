26840fda8186291192000aa9e79c9c172b0ca1f2
SAVERIO <109146940+SaverioPalmiero@users.noreply.github.com>
2022-07-14 17:10:23 +0200

    Add files via upload

---
+++ calcolo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main ()
+{
+	int base;
+	int altezza;
+	int area;
+	
+	base=7;
+	altezza=3;
+	area= base*altezza;
+	
+	printf("base:7 \n");
+	printf("altezza:3 \n");
+	printf("\n risultato %d",area);
+	getchar();
+}
