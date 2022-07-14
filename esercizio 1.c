2b0319357969863f2975ae56e8a314ec9e476fe7
SAVERIO <109146940+SaverioPalmiero@users.noreply.github.com>
2022-07-14 11:38:25 +0200

    Add files via upload

---
+++ esercizio 1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main() {
+	
+	/*Dichiarazione Variabili*/
+	int onlynumber,  //numero preso da input tastiera
+	int numberoIncrementato;
+	int nnumeroDecrementato;
+	
+	/*Richiesta all'utente di un numero*/
+	printf("Insercisci numero\n");
+	scanf("%d",&onlynumber);
+	
+	
+	/*Calcoli di incremento/decremento*/
+	numeroIncrementato = onlynumber +1;
+	numeroDecrementato = onlynumber -1;
+	
+	/*Stampa a video*/
+	printf("numberplus: %d\n",numeroIncrementato);
+	printf("numbermemeno: %d",numeroDecrementato);
+	
+	return 0;
+	
+	
+
+	}
+	
+
+	
+	
+
+ 
+ 
+	
+	
+	
+	
+
+
