c0c2d85fd1a0aee0950668b791f2904154d918b0
SAVERIO <109146940+SaverioPalmiero@users.noreply.github.com>
2022-07-14 14:33:01 +0200

    Add files via upload

---
+++ esercizio 2,2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+
+
+int numero0;
+int numero5;
+int numero8;
+
+printf("Inserisci numero");
+scanf("%d",&numero5);
+
+printf("Inserisci numwero");
+scanf("%d",&numero8);
+
+media= numero5+numero8/2
+
+
+
+printf("numero0:%d\n",numero5);
+printf ("numero0:%d\n",numero8):
+	return 0;
+
