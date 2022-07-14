56793c03a196f6f238bb1713d1f0bc990557d7ef
SAVERIO <109146940+SaverioPalmiero@users.noreply.github.com>
2022-07-14 17:07:10 +0200

    Add files via upload

---
+++ esercizio 2,2 corretto.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+
+int somma;
+int numero5;
+int numero8;
+
+printf("Inserisci numero");
+scanf("%d",&numero5);
+
+printf("Inserisci numero");
+scanf("%d",&numero8);
+
+somma= numero5+numero8;
+
+
+
+printf("La somma dei due numeri e' la seguente;%d",somma);
+	return 0;
+	
+}
+
